e107689dc01bd8c4ee3d260e69e08f5a3827273c
emrprogg <arasemr1234@gmail.com>
2020-12-15 16:44:30 +0300

    0.0.1

---
+++ script.js
@@ -0,0 +1,14 @@
+//DİSABLE CONTEXTMENU
+$(document).on("contextmenu", function (e) {        
+    e.preventDefault();
+});
+//DİSABLE F12&CTRL+SHİFT+I
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) {
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {   
+        return false;
+    } else if (event.ctrlKey && event.keyCode == 67){
+        return false;
+    }
+});
